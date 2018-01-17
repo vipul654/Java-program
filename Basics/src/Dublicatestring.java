a23822103e98415b782dccf0b1df58f7e0accd1b
pc <pc@DESKTOP-46T0MN5>
2018-01-17 16:42:12 +0530

    Duplicate

---
+++ Basics/src/Dublicatestring.java
@@ -0,0 +1,24 @@
+
+public class Dublicatestring {
+
+	public static void main(String[] args) 
+	{
+    int arr[]= {1,2,2,3,3,4,5}	;
+       
+       for(int i=0; i<=arr.length;i++ )
+    	   
+    	   for (int j = i+1; j < arr.length; j++) 
+    	   {
+			 if (arr[i]==arr[j])
+				 
+				 System.out.println(arr[i]);
+				
+			}
+		}
+       
+       
+       
+
+	}
+
+
